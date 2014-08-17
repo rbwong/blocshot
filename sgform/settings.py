caba0ae8fa074e8405dac1f94ed0cc7d368f4cf9
rbwong <rbwong@up.edu.ph>
2014-08-18 01:22:16 +0800

    settings

--- sgform/settings.py
+++ sgform/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'sgform.urls'
