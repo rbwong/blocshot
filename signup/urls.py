3c216495eb29c68c74bd21571ee0903e4132ea8f
rbwong <rbwong@up.edu.ph>
2014-08-19 08:01:32 +0800

    stable

--- signup/urls.py
+++ signup/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from signup.views import SignupPage
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', SignupPage.as_view(), name='signup_page'),
+                       (r'^channel/', login_required(TemplateView.as_view(template_name="channel.html"))),
                        )
