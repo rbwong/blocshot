aae8d5d2b5b11665ec4b950f994d36ad2b0564a2
rbwong <rbwong@up.edu.ph>
2014-08-19 08:39:59 +0800

    signup

--- sgform/settings.py
+++ sgform/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'fb_iframe.middleware.FacebookMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 )
 
 ROOT_URLCONF = 'sgform.urls'
