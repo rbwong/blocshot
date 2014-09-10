8124070d38407e318f2f477031fce9ab3f90a8d2
rbwong <rbwong@up.edu.ph>
2014-09-11 00:22:35 +0800

    removed fb shit

--- sgform/settings.py
+++ sgform/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
