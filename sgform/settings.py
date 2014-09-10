05a0c8e64cace615bba4d759a82f6cc32d0169bc
rbwong <rbwong@up.edu.ph>
2014-09-11 00:28:38 +0800

    removed fb shit

--- sgform/settings.py
+++ sgform/settings.py
@@ -102,6 +102,9 @@ TEMPLATE_DIRS = (
     TEMPLATE_PATH,
 )
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -113,3 +116,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
